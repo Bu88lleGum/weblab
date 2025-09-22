480ed9e60869f1cacadbb0f57e4e6de20d365ace
Kostya <kostya.permin2007@gmail.com>
2025-09-22 18:34:26 +0500

    Initial commit - Nuxt project

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="relative w-full border-b-2 hover:shadow-md transition-shadow duration-500 bg-brand-background_white ">
+    <!-- logo -->
+    <div class="flex justify-center pt-4">
+      <img src="assets/images/Component 1.svg" alt="Site Logo" class="w-20 h-20 rounded-full shadow-lg bg-brand-primary p-2 mt-2 mb-2" />
+    </div>
+
+    <!-- nav -->
+    <nav class="flex justify-center gap-8 px-6 py-4 text-lg font-medium text-brand-background_white">
+      <NuxtLink to="/" class="hover:bg-brand-accent transition bg-brand-primary rounded-xl p-2 duration-500">Home</NuxtLink>
+      <NuxtLink to="/lab3" class="hover:bg-brand-accent transition bg-brand-primary rounded-xl p-2 duration-500">Lab3</NuxtLink>
+      <NuxtLink to="/login" class="hover:bg-brand-accent transition  bg-brand-primary rounded-xl p-2 duration-500">LogIn</NuxtLink>
+      <NuxtLink to="/logout" class="hover:bg-brand-accent transition bg-brand-primary rounded-xl p-2 duration-500" >LogOut</NuxtLink>
+    </nav>
+  </header>
+  
+  <main class="p-5 flex bg-brand-background_white h-screen">
+    <slot/>
+  </main>
+  
+  <footer class="flex flex-row w-full bg-brand-primary border-t-2 justify-center px-4 py-2 gap-4">
+    <a href=""></a>
+    <a href=""></a>
+    <a href=""></a>
+  </footer>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          // Accent color: use for highlights, buttons, hover states
+          accent: "#E63946",
+
+          // Background (main page background, very light tone)
+          background_white: "#F1FAEE",
+
+          // Surface color: use for cards, sections, panels
+          surface: "#A8DADC",
+
+          // Primary brand color: use for header, footer, key UI areas
+          primary: "#457B9D",
+
+          // Text color: main color for typography
+          text: "#1D3557",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
